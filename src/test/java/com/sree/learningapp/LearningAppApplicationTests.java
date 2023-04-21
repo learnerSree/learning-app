17fe2693d8250cb6ce70f4f763803bd269a92d1e
Sreejesh Sreekumar <sreejeshsreekumar19@gmail.com>
2023-04-21 18:01:06 +0530

    aded java files

---
+++ src/test/java/com/sree/learningapp/LearningAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sree.learningapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearningAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
